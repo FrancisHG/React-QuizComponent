bfc950a57009a58f37717a30a0c8c9c424a74df5
Francis Gelderloos <fgelderl@mail.usf.edu>
2020-08-23 15:00:10 -0400

    Update Quiz.js

--- src/Quiz.js
+++ src/Quiz.js
@@ -23,7 +23,7 @@ class Quiz extends Component
     //const isQuizEnd = true;
     return <div>
       {isQuizEnd ?
-        <QuizEnd />
+        <QuizEnd resetClickHandler={this.handleResetClick.bind(this)} />
         : <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position-1]} showNextQuestionHandler={this.showNextQuestion.bind(this)}/>}
       </div>
   }
